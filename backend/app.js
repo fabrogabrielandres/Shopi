9ed0592d38698842ba775ad487700b1e1fc61af6
FabroGabriel <fabrogabrielandres@gmail.com>
2021-09-11 13:08:03 -0500

    Products Route 3.3

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,16 @@ const express = require("express");
 const app = express();
 
 
+
+//lectura y parceo del body (middlewares)
+app.use(express.json())
+
+//import all routes
+const products = require("./routes/products") 
+
+app.use("/api/v1",products)
+
+
+
 module.exports = app
 
