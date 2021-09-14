1abbaac1b91f9d5e5f8a34462eb813047630383f
FabroGabriel <fabrogabrielandres@gmail.com>
2021-09-14 15:55:15 -0500

    new test products were created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,8 +6,8 @@ exports.newProduct = async (req, res, next) =>{
     const product = await Product.create(req.body)
 
     res.status(201).json({
-        success: true ,
-        product
+        success: true,
+        product        
     })
 }  
 
