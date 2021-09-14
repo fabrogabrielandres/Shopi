fafdff1bf6ff636c8cca3c72e70549d4863148ba
FabroGabriel <fabrogabrielandres@gmail.com>
2021-09-14 17:22:12 -0500

    funtion get AllProducts

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,11 +11,15 @@ exports.newProduct = async (req, res, next) =>{
     })
 }  
 
+//get all products => /api/v1/products
+exports.getProducts = async (req, res, next)=> {
 
-exports.getProducts = (req, res, next)=> {
+    const products = await Product.find();
+    
     res.status(200).json({
         success:true,
-        message:"this route show all products in database"
+        count: products.length,
+        products
     })
 }
 
